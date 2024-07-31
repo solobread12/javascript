e5ad2305047f58241dd7a6965eacc95a163b2816
solobread12 <160695947+solobread12@users.noreply.github.com>
2024-07-31 10:49:29 +0700

    meeting 2

---
+++ JAVASCRIPT/meeting 2/coba(2).node.js
@@ -0,0 +1,20 @@
+// tugas no 1
+var b;
+console.log(b);
+// batas
+// tugas no 2
+var nama = " abyan";
+var umur = " 16";
+var tmptinggal = " bekasi";
+console.log(nama + umur + tmptinggal);
+// batas
+// tugas no 3
+var a = null;
+console.log(a);
+// batas
+// tugas no 4
+let alas = 5;``
+let tinggi = 7;
+let rumus = 1/2
+console.log(alas*tinggi*rumus)
+// batas
